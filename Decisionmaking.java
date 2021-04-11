211534efbeda4731b525529aea754a66a163f18b
Ankita2105 <62275934+Ankita2105@users.noreply.github.com>
2021-04-11 20:50:55 +0530

    Add files via upload

---
+++ Decisionmaking.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Decisionmaking
+{
+	public static void main(String[] args)
+	{
+		int input;
+		System.out.println("Enter your age :");
+		Scanner sc = new Scanner(System.in);
+		input = sc.nextInt();
+		if(input > 18)
+		{
+			System.out.println("You are eligible to vote");
+		}
+		else if(input > 20)
+		{
+			System.out.println("You are a major");
+		}
+		else{
+			System.out.println("You are NOT a major");
+		}
+	}
+}
